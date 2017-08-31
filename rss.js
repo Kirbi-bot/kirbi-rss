f7f81276d22ad27fb815e76fae5dc6454cfa4cb5
Nate Richardson <hello@naterichardson.com>
2017-08-31 00:42:32 -0400

    linting

--- rss.js
+++ rss.js
@@ -1,18 +1,16 @@
-const fs = require('fs');
 const FeedParser = require('feedparser');
-const feedparser = new FeedParser();
 const request = require('request');
-const chalk = require('chalk');
 
 module.exports = function (Kirbi) {
-	let rssFeeds = Kirbi.getJsonObject('/config/rss.json');
-	let returnObject = {
-		commands: [ 'rss' ],
+	const feedparser = new FeedParser();
+	const rssFeeds = Kirbi.getJsonObject('/config/rss.json');
+	const returnObject = {
+		commands: ['rss'],
 		rss: {
 			description: 'Lists Available RSS Feeds',
-			process: function (msg, suffix, isEdit, cb) {
+			process: (msg, suffix, isEdit, cb) => {
 				let output = '';
-				for (let c in rssFeeds) {
+				for (const c in rssFeeds) {
 					output += `${c}: ${rssFeeds[c].url}\n`;
 				}
 				cb(`Available feeds:\n${output}`, msg);
@@ -20,15 +18,15 @@ module.exports = function (Kirbi) {
 		}
 	};
 	function loadFeeds() {
-		for (let cmd in rssFeeds) {
+		for (const cmd in rssFeeds) {
 			returnObject.commands.push(cmd);
 			returnObject[cmd] = {
 				usage: '[count]',
 				description: rssFeeds[cmd].description,
 				url: rssFeeds[cmd].url,
-				process: function (msg, suffix, isEdit, cb) {
+				process: (msg, suffix, isEdit, cb) => {
 					let count = 1;
-					if (suffix != null && suffix != "" && !isNaN(suffix)) {
+					if (suffix !== null && suffix !== '' && !isNaN(suffix)) {
 						count = suffix;
 					}
 					rssfeed(msg, this.url, count, cb);
@@ -38,17 +36,17 @@ module.exports = function (Kirbi) {
 	}
 	function rssfeed(msg, url, count, cb) {
 		request(url).pipe(feedparser);
-		feedparser.on('error', function (error) {
+		feedparser.on('error', error => {
 			cb(`Failed reading feed: ${error}`, msg);
 		});
 		let shown = 0;
 		feedparser.on('readable', function () {
-			let stream = this;
-			shown += 1
+			const stream = this;
+			shown += 1;
 			if (shown > count) {
 				return;
 			}
-			let item = stream.read();
+			const item = stream.read();
 			cb(`${item.title} ${item.link}`, msg);
 			stream.alreadyRead = true;
 		});
